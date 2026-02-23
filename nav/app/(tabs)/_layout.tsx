ca7366503a25e2a8b6e5e7c2f87429c2bb22ab8c
Kevin Liell <kevincamposliell@gmail.com>
2026-02-23 16:03:57 -0300

    1

---
+++ nav/app/(tabs)/_layout.tsx
@@ -0,0 +1,17 @@
+import { Tabs } from 'expo-router'
+import { Ionicons } from "@expo/vector-icons"
+
+const TabsLayout = () => {
+    return (<Tabs>
+        <Tabs.Screen name='index' options={
+            {title: "Home",
+            tabBarIcon: ({ color }) => (
+        <Ionicons name='home' cloor={color} />
+    )}} />
+        <Tabs.Screen name='catalogo' options={{ title: "Shop" }} />
+    </Tabs>
+    )
+}
+
+
+export default TabsLayout
